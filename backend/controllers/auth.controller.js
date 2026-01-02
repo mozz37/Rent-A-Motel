8d9aa05d757d70612c2163cc0c957134fec49a51
mozz37 <hero1cnvp@gmail.com>
2026-01-02 21:42:05 +0530

    backend setup with signup endpoint

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,49 @@
+import bcryptjs from "bcryptjs"
+import prisma from "../db/prisma.js"
+
+export const signup = async (req, res) => {
+    try {
+        const { username, fullName, email, password, confirmPassword } = req.body
+
+        if (!fullName || !username || !email || !password || !confirmPassword) {
+			return res.status(400).json({ error: "Please fill in all fields" })
+		}
+
+        if (password !== confirmPassword) {
+			return res.status(400).json({ error: "Passwords do not match" })
+		}
+
+        const user = await prisma.user.findUnique({ where: { username } })
+
+        if (user) {
+			return res.status(400).json({ error: "User already exists" })
+		}
+
+        const salt = await bcryptjs.genSalt(10)
+		const hashedPassword = await bcryptjs.hash(password, salt)
+
+        const newUser = await prisma.user.create({
+			data: {
+                username,
+				fullName,
+                email,
+				password: hashedPassword,
+			},
+		})
+
+        if (newUser) {
+			generateToken(newUser.id, res)
+			res.status(201).json({
+				id: newUser.id,
+				fullName: newUser.fullName,
+				username: newUser.username,
+                email: newUser.email
+			})
+		} else {
+			console.log("Error in signup controller", error.message)
+		    res.status(500).json({ error: "Internal Server Error" })
+		}
+    } catch (error) {
+        console.log(error)
+    }
+}
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api", authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
